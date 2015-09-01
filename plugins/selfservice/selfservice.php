dc738306501ab7c06ee5c5a61e28bf78845acfbb
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2015-09-01 15:48:18 +0000

    fix code errors in plugin; added test-setup script; init dev db for wordpress tests

--- plugins/selfservice/selfservice.php
+++ plugins/selfservice/selfservice.php
@@ -7,35 +7,40 @@ Author:      Chris Greenhalgh
 Author URI:  http://www.cs.nott.ac.uk/~cmg
 License:     AGPLv3 or later
 License URI: http://www.gnu.org/licenses/agpl-3.0.en.html
-*/
+@package wpss
+ */
+
 /*
 Copyright (c) 2015, The University of Nottingham
 */
+
 defined( 'ABSPATH' ) or die( 'This is a plugin' );
 
 add_action( 'init', 'wpss_create_post_types' );
-//Register the app post type
+/**
+ * Register the app post type.
+ */
 function wpss_create_post_types() {
-    register_post_type( 'wpss_site',
-        array(
-            'labels' => array(
-                'name' => __( 'Website' ),
-                'singular_name' => __( 'Website' ),
-                'add_new_item' => __( 'Add New Website' ),
-                'edit_item' => __( 'Edit Website' ),
-                'new_item' => __( 'New Website' ),
-                'view_item' => __( 'View Website Info' ),
-                'search_items' => __( 'Search Websites' ),
-                'not_found' => __( 'No Website found' ),
-                'not_found_in_trash' => __( 'No Website found in Trash' ),
-                'all_items' => __( 'All Websites' )
-            ),
-            'description' => __( 'WordPress SelfService Website' ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array( 'title', 'editor', 'author', 'revisions', 'comments', 'thumbnail' ),
-	    'menu_icon' => 'dashicons-palmtree',
-        )
-    );
+	register_post_type( 'wpss_site',
+		array(
+			'labels' => array(
+				'name' => __( 'Website' ),
+				'singular_name' => __( 'Website' ),
+				'add_new_item' => __( 'Add New Website' ),
+				'edit_item' => __( 'Edit Website' ),
+				'new_item' => __( 'New Website' ),
+				'view_item' => __( 'View Website Info' ),
+				'search_items' => __( 'Search Websites' ),
+				'not_found' => __( 'No Website found' ),
+				'not_found_in_trash' => __( 'No Website found in Trash' ),
+				'all_items' => __( 'All Websites' ),
+			),
+			'description' => __( 'WordPress SelfService Website' ),
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array( 'title', 'editor', 'author', 'revisions', 'comments', 'thumbnail' ),
+			'menu_icon' => 'dashicons-palmtree',
+		)
+	);
 }
 
--- saltstack/salt/php-wordpress-dev.sls
+++ saltstack/salt/php-wordpress-dev.sls
@@ -3,11 +3,38 @@
 include:
   - php.composer
 
-php-codesniffer:
+php-wp-coding-standards:
   cmd.run:
-   - name: composer create-project wp-coding-standards/wpcs:dev-master --no-dev
+   - name: composer create-project wp-coding-standards/wpcs:dev-master --no-dev --keep-vcs
    - cwd: /srv
    - creates: /srv/wpcs/vendor/bin/phpcs
    - require:
       - cmd: install-composer
-  
+
+/usr/local/bin/phpcs:
+  file.symlink:
+   - target: /srv/wpcs/vendor/bin/phpcs
+   - requires:
+      - cmd: php-wp-coding-standards
+
+# dev database
+wordpress-dev db:
+  mysql_database.present:
+   - name: wordpress-dev
+
+wordpress-dev user:
+  mysql_user.present:
+   - name: wordpress-dev
+   - allow_passwordless: True
+   - host: localhost
+
+wordpress-dev grant:
+  mysql_grants.present:
+   - user: wordpress-dev
+   - database: wordpress-dev.*
+   - host: localhost
+   - grant: select,insert,update,delete,create,drop,alter,index
+   - require:
+      - mysql_database: wordpress-dev
+      - mysql_user: wordpress-dev
+
